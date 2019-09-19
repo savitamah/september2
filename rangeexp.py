14da3ea1958de4f726f407258b3d052c397687ca
Ashwani <aswanibtech@gmail.com>
2019-09-19 15:36:35 +0530

    wending machine numberguessing game while for

---
+++ rangeexp.py
@@ -0,0 +1,12 @@
+import time
+print(list(range(1,10,2)))
+'''range(1,10)
+range(1,20,2)'''
+
+for i in range(6):
+    time.sleep(1)
+    print(i)
+
+lst=["Ram",1,4,7,9]
+for i in lst:
+    print(i)
---
+++ whileexp.py
@@ -0,0 +1,20 @@
+'''i=1
+while i<=10:
+    print(i,"Happy B-day")
+    i=i+1'''
+
+'''a=int (input("enter any number"))
+i=1
+while i<=10:
+    print(a,"*",i,"=",i*a)
+    i=i+1'''
+
+
+
+i=1
+while i<=10:
+    if i%2==0:
+       pass
+    else:
+        print("Odd no=", i)
+    i=i+1
